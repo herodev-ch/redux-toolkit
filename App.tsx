0c9dd223a95ceb43839e5930277795571a8dbf86
ahmedsawy1 <sawy22417@gmail.com>
2023-11-09 16:17:02 +0200

    remove old redux

--- App.tsx
+++ App.tsx
@@ -2,16 +2,12 @@ import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import MainTabs from './src/navigation/MainTabs';
 import {NavigationContainer} from '@react-navigation/native';
-import { Provider } from 'react-redux';
-import { store } from './src/store/store';
 
 const App = () => {
   return (
-    <Provider store={store}>
       <NavigationContainer>
         <MainTabs />
       </NavigationContainer>
-    </Provider>
   );
 };
 
--- src/store/types.ts
+++
@@ -1,4 +0,0 @@
-export const LOGIN = "LOGIN"
-export const LOGOUT = "LOGOUT"
-export const CHANGE_NAME = "CHANGE_NAME"
-export const GET_USER_BOOKS = "GET_USERS_BOOKS"
